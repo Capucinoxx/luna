3a55cdbda3509ab60dce3c2f723d2aae7ffce0a3
Capucinoxx <bendertermebc@gmail.com>
2022-08-25 03:00:28 -0400

    [feat] - fmt affichage pour les tests

--- tests/test.c
+++ tests/test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "stats.h"
-#include "../data-structure/test_skip-list.h"
+#include "../data-structure/skip-list_test.h"
 #include "../data-structure/storage_test.h"
 
 void print_hdr(char *title) {
@@ -13,6 +13,7 @@ int main() {
   print_hdr("test skipList");
   test_skipList();
   puts(DIV);
+  puts("");
 
   print_hdr("test storage");
   test_storage();
